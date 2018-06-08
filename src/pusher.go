52fd8194c6ea6af597c2926a13fa38987791eba0
BenGiese22 <bengiese22@gmail.com>
2018-06-08 13:53:42 -0500

    updated

--- src/pusher.go
+++ src/pusher.go
@@ -8,13 +8,12 @@ import (
         "net/http"
         "os"
         "strings"
+        "strconv"
         "net/smtp"
         "time"
-
         "golang.org/x/net/context"
         "golang.org/x/oauth2"
-
-
+        "github.com/aws/aws-lambda-go/lambda"
 )
 
 type ProspectDetailResponse struct {
@@ -101,32 +100,118 @@ func saveToken(path string, token *oauth2.Token) {
 }
 
 func main() {
-        now := time.Now().In(time.FixedZone("CDT", -18000))
-        subtracted_time := now.Add(-10*time.Minute)
-        //log.Println("Now: " + now.String() +  " .... " + "Sub: " + subtracted_time.String())
 
-        str_now := now.Format("2006-01-02 15:04:05")
-        sub_now := subtracted_time.Format("2006-01-02 15:04:05")
 
-        prospects := GetProspectDetails(sub_now,str_now)
-        for _, val := range prospects.Response.Data {
-            if (val.ShowingWasScheduled == "t") {
-              log.Println("Lead Email: " + val.Email + "  Agent: " + val.TeamMember)
-              //send(val.Address + " " + val.Unit, val.Name, val.Email, val.Phone,val.TeamMember)
-            }
-          }
+  lambda.Start(Action)
 
 }
 
+func Action() (error) {
+  start, end := getTimes()
+
+  str_start := timeForShowMojo(start)
+  str_end := timeForShowMojo(end)
+
+  log.Println(start.String(),end.String())
+
+  prospects := GetProspectDetails(str_start,str_end)
+    for _, val := range prospects.Response.Data {
+
+      //Created: 7 Jun 2018, 6:58AM  CDT
+      v := createdAtToTime(val.CreatedAt)
+
+
+      if(v.After(start)) {
+        if (val.ShowingWasScheduled == "t") {
+          log.Println("Lead Email: " + val.Email + "  Agent: " + val.TeamMember + "  Created: " + val.CreatedAt)
+          //send(val.Address + " " + val.Unit, val.Name, val.Email, val.Phone,val.TeamMember)
+        }
+      }
+    }
+	return nil
+}
+
+//Takes in the parameter of val.CreatedAt
+//Converts it to a working time object/Struct
+//Returns that time object/Struct
+func createdAtToTime(created string) (time.Time) {
+  //Created: 7 Jun 2018, 11:58AM  CDT
+  str := strings.Replace(created, ",","",-1)
+  //Created: 7 Jun 2018 11:58AM  CDT
+  split_str := strings.Split(str," ")
+  //[7][Jun][2018][11:58AM][CDT]
+
+  //Fix time
+  num_time := split_str[3]
+  indicator := num_time
+  indicator = indicator[len(indicator)-2:]
+  num_time = num_time[0:(len(num_time)-2)]
+  time_split := strings.Split(num_time,":")
+  hr,_ := strconv.Atoi(time_split[0])
+  if indicator == "AM" && hr == 12 {
+    hr = 0
+  } else if indicator == "PM" && hr != 12 {
+    hr = hr + 12
+  }
+
+  hr = hr + 5
+
+  _,month,_ := time.Now().Date()
+
+  year_str := split_str[2]
+  year,_ := strconv.Atoi(year_str)
+  day_str := split_str[0]
+  day,_ := strconv.Atoi(day_str)
+  min_str := time_split[1]
+  min,_ := strconv.Atoi(min_str)
+
+  if hr > 23 {
+    hr = hr - 23
+    day = day + 1
+  }
+
+  //create Time Struct
+  t := time.Date(year,month,day,hr,min,0,0,time.UTC)
+  return t
+}
+
+//Returns the time of method call and 15 minutes before that.
+func getTimes() (time.Time, time.Time) {
+  time_now := time.Now().UTC()
+  subtracted_time := time_now.Add(-15*time.Minute)
+  return subtracted_time,time_now
+}
+
+//Converts time to string value time for showmojo
+func timeForShowMojo(v time.Time) (string) {
+
+	day := strconv.Itoa(v.Day())
+	if v.Day() < 10 {
+		day = "0" + day
+	}
+	month := strconv.Itoa(int(v.Month()))
+	if int(v.Month()) < 10 {
+		month = "0" + month
+	}
+	minute := strconv.Itoa(int(v.Minute()))
+	if v.Minute() < 10 {
+		minute = "0" + minute
+	}
+	hour := strconv.Itoa(int(v.Hour()))
+	if v.Hour() < 10 {
+		hour = "0" + hour
+	}
+	return strconv.Itoa(v.Year()) + "-" + month + "-" + day + " " + hour + ":" + minute + ":00"
+}
 
 //Sends email with specified Lead Info. to the LeadSimple Email Importer
 //LS Email: new-leadd564b4aaf8@newlead.leadsimple.com
+//Zapier Email: i5qc1rg9@robot.zapier.com
 func send(address string, name string, email string, phone string, agent string) {
-  //TODO when change to pammke email, need to enable access to less secure apps.
   agent_name := getLeadSimpleName(agent)
-  from := "bengiese22@gmail.com"
-  pass := "Toddwodd1"
-  to := "bengiese22@gmail.com"
+  from := "Show2Lead@gmail.com"
+  pass := "Miller2179"
+  to := "i5qc1rg9@robot.zapier.com"
   body := "Address: " + address + "\r\n" + "Name: " + name + "\r\n" +
   "Phone: " + phone + "\r\n" + "Email: " + email + "\r\n" + "Assign To: " + agent_name + "\r\n"
 
@@ -165,7 +250,16 @@ func getLeadSimpleName(agent string) string {
   if len(split) < 2 {
     return agent
   } else {
-    return split[1]
+    switch split[1] {
+    case "Matt":
+      return "5610"
+    case "Gino":
+      return "5611"
+    case "Shawn":
+      return "0"
+    default:
+      return "0"
+    }
   }
 }
 
