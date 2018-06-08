707be51e3bb8f0086798184db847fcc311d971fc
BenGiese22 <bengiese22@gmail.com>
2018-06-08 09:54:08 -0500

    First commit

---
+++ src/pusher.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        "strings"
+        "net/smtp"
+        "time"
+
+        "golang.org/x/net/context"
+        "golang.org/x/oauth2"
+
+
+)
+
+type ProspectDetailResponse struct {
+	Response struct {
+		Status string `json:"status"`
+		Data []struct {
+			CreatedAt            string `json:"created_at"`
+			Name                 string `json:"name"`
+			Email                string `json:"email"`
+			Phone                string `json:"phone"`
+			Showtime             string `json:"showtime"`
+			Address              string `json:"Address"`
+			Unit                 string `json:"Unit"`
+			ShowingWasScheduled  string `json:"showing_was_scheduled"`
+			ShowingMethod        string `json:"showing_method"`
+			NoShow               string `json:"no_show"`
+			CurrentStatus        string `json:"current_status"`
+			LeadSource           string `json:"lead_source"`
+			ReferrerURL          string `json:"referrer_url"`
+			SourceEmailAutoreply string `json:"source_email_autoreply"`
+			SourceShowmojoPhone  string `json:"source_showmojo_phone"`
+			TeamMember           string `json:"team_member"`
+			Comments             string `json:"comments"`
+			Answer8151           string `json:"answer_8151"`
+			Answer8152           string `json:"answer_8152"`
+			Answer8153           string `json:"answer_8153"`
+			Answer8261           string `json:"answer_8261"`
+			Answer8263           string `json:"answer_8263"`
+			Answer8275           string `json:"answer_8275"`
+		} `json:"data"`
+	} `json:"response"`
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+        tokFile := "token.json"
+        tok, err := tokenFromFile(tokFile)
+        if err != nil {
+                tok = getTokenFromWeb(config)
+                saveToken(tokFile, tok)
+        }
+        return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+        fmt.Printf("Go to the following link in your browser then type the "+
+                "authorization code: \n%v\n", authURL)
+
+        var authCode string
+        if _, err := fmt.Scan(&authCode); err != nil {
+                log.Fatalf("Unable to read authorization code: %v", err)
+        }
+
+        tok, err := config.Exchange(oauth2.NoContext, authCode)
+        if err != nil {
+                log.Fatalf("Unable to retrieve token from web: %v", err)
+        }
+        return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+        f, err := os.Open(file)
+        defer f.Close()
+        if err != nil {
+                return nil, err
+        }
+        tok := &oauth2.Token{}
+        err = json.NewDecoder(f).Decode(tok)
+        return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+        fmt.Printf("Saving credential file to: %s\n", path)
+        f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+        defer f.Close()
+        if err != nil {
+                log.Fatalf("Unable to cache oauth token: %v", err)
+        }
+        json.NewEncoder(f).Encode(token)
+}
+
+func main() {
+        now := time.Now().In(time.FixedZone("CDT", -18000))
+        subtracted_time := now.Add(-10*time.Minute)
+        //log.Println("Now: " + now.String() +  " .... " + "Sub: " + subtracted_time.String())
+
+        str_now := now.Format("2006-01-02 15:04:05")
+        sub_now := subtracted_time.Format("2006-01-02 15:04:05")
+
+        prospects := GetProspectDetails(sub_now,str_now)
+        for _, val := range prospects.Response.Data {
+            if (val.ShowingWasScheduled == "t") {
+              log.Println("Lead Email: " + val.Email + "  Agent: " + val.TeamMember)
+              //send(val.Address + " " + val.Unit, val.Name, val.Email, val.Phone,val.TeamMember)
+            }
+          }
+
+}
+
+
+//Sends email with specified Lead Info. to the LeadSimple Email Importer
+//LS Email: new-leadd564b4aaf8@newlead.leadsimple.com
+func send(address string, name string, email string, phone string, agent string) {
+  //TODO when change to pammke email, need to enable access to less secure apps.
+  agent_name := getLeadSimpleName(agent)
+  from := "bengiese22@gmail.com"
+  pass := "Toddwodd1"
+  to := "bengiese22@gmail.com"
+  body := "Address: " + address + "\r\n" + "Name: " + name + "\r\n" +
+  "Phone: " + phone + "\r\n" + "Email: " + email + "\r\n" + "Assign To: " + agent_name + "\r\n"
+
+  msg := "From: " + from + "\n" +
+		"To: " + to + "\n" +
+		"Subject: New Lead\n\n" +
+		body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Printf("smtp error: %s", err)
+		return
+	}
+
+	log.Print("sent, visit "+ to)
+}
+
+//Returns ProspectDetailResponse Type (See Struct Above) with Prospect info.
+func GetProspectDetails(startTime string, endTime string) (ProspectDetailResponse) {
+	body2 := readProspects(startTime, endTime)
+	//println(body2)
+	var response ProspectDetailResponse
+	json.Unmarshal(body2, &response)
+	return response
+}
+
+//Takes in Agent Name from Showmojo (Gino P / Matt M) and converts
+//it to LeadSimple format (Gino / Matt)
+func getLeadSimpleName(agent string) string {
+  //Matt M -> Matt
+  split := strings.Split(agent," " )
+  log.Print(split[1])
+  if len(split) < 2 {
+    return agent
+  } else {
+    return split[1]
+  }
+}
+
+//Calls Post to the prospect data and gets returned raw data
+func readProspects(startDate string, endDate string) []byte {//todo handle errs
+	// Generated by curl-to-Go: https://mholt.github.io/curl-to-go
+	//body := strings.NewReader(`start_date=2017-11-20 19:00:00&end_date=2017-11-20 20:00:00`)
+	body := strings.NewReader(`start_date=` + startDate + `&end_date=` + endDate)
+
+  //requests the prospect data
+	req, err := http.NewRequest("POST", "https://showmojo.com/api/v3/reports/detailed_prospect_data", body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Authorization", "Token token=\"ec6b7cb47c6f1270949f16215227afd9\"") //todo add to configuration
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// handle err
+	}
+	defer resp.Body.Close()
+	body2, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		//handle err
+	}
+	return body2
+}
